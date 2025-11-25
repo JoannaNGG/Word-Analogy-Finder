baa2b7ac2ad874f939804598c1387a3be48a3a37
JoannaNGG <Joanna.456.ng@gmail.com>
2025-11-25 18:16:59 +0000

    Created new file to compare the vector distance

---
+++ CompareVecDistance.py
@@ -0,0 +1,31 @@
+import gensim.downloader as api
+
+print("\nLoading the Word2Vec Model, please wait...")
+model = api.load("word2vec-google-news-300")
+print("Model Loaded.\n")
+
+analogy = input("Enter your analogy in format 'A:B::C:?'\n")
+
+left, right = analogy.split("::")
+A, B = left.split(":")
+C, _ = right.split(":")
+
+#Compute B - A + C
+result = model.most_similar(
+    positive=[B.strip(), C.strip()],
+    negative=[A.strip()],
+    topn=5
+)
+
+roundResult = [(word, round(score, 3)) for word, score in result]
+print(roundResult)
+
+#Compare top 1 result with the word "male/female"
+top_word = result[0][0]  #top result
+
+sim_male = model.similarity(top_word, "male")
+sim_female = model.similarity(top_word, "female")
+
+print(f"\nSimilarity of '{top_word}' to 'male':   {round(sim_male, 3)}")
+print(f"Similarity of '{top_word}' to 'female': {round(sim_female, 3)}")
+
